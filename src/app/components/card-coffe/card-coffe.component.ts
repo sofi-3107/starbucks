3f33dec4087179a2d4098552e34a25cb949a2d99
sofi-3107 <62622196+sofi-3107@users.noreply.github.com>
2022-09-01 17:12:16 -0300

    lista de productos desde api

--- src/app/components/card-coffe/card-coffe.component.ts
+++ src/app/components/card-coffe/card-coffe.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-coffe',
@@ -9,6 +9,13 @@ export class CardCoffeComponent implements OnInit {
 
   cantidad:number=0;
   disponible:number=10;
+  @Input()
+  nombre?:string;
+  @Input()
+  imagen?:string;
+  @Input()
+  precio?:number;
+  
   constructor() { }
 
   ngOnInit(): void {
